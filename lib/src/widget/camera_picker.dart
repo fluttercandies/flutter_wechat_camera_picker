d150ac956253960b1d90feac69ffeb5ca9b4f610
Alex Li <github@alexv525.com>
2021-09-16 11:11:15 +0800

    🚸 Flip the preview if the user is using a front camera

--- lib/src/widget/camera_picker.dart
+++ lib/src/widget/camera_picker.dart
@@ -1237,6 +1237,14 @@ class CameraPickerState extends State<CameraPicker>
     }
     final double _offsetHorizontal = (_width - constraints.maxWidth).abs() / -2;
     final double _offsetVertical = (_height - constraints.maxHeight).abs() / -2;
+    // Flip the preview if the user is using a front camera to match the result.
+    if (currentCamera.lensDirection == CameraLensDirection.front) {
+      _preview = Transform(
+        transform: Matrix4.rotationY(math.pi),
+        alignment: Alignment.center,
+        child: _preview,
+      );
+    }
     _preview = Stack(
       children: <Widget>[
         Positioned(
