b93a10fb680f1daf4f98a0a9fd0715651c33d869
Alex Li <github@alexv525.com>
2021-10-12 11:45:23 +0800

    ✨ Add `CameraErrorHandler` (#52)

--- lib/src/internals/methods.dart
+++ lib/src/internals/methods.dart
@@ -6,10 +6,21 @@ import 'dart:developer';
 
 import 'package:flutter/foundation.dart';
 
+import 'extensions.dart';
+import 'type_defs.dart';
+
 /// Log only in debug mode.
 /// 只在调试模式打印
 void realDebugPrint(dynamic message) {
   if (!kReleaseMode) {
     log('$message', name: 'CameraPicker - LOG');
   }
+}
+
+void handleErrorWithHandler(Object error, CameraErrorHandler? handler) {
+  if (handler != null) {
+    handler(error, error.nullableStackTrace);
+    return;
+  }
+  throw error;
 }
