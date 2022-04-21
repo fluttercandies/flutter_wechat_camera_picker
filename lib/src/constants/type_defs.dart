3effc8e6cfc58c945e615564f35fb870418c9483
Alex Li <github@alexv525.com>
2022-04-21 10:50:51 +0800

    ♻️ Make `XFileCapturedCallback` returns a boolean result (#89)

--- lib/src/constants/type_defs.dart
+++ lib/src/constants/type_defs.dart
@@ -63,11 +63,14 @@ typedef PreviewTransformBuilder = Widget? Function(
 /// The callback type definition when the XFile is captured by the camera.
 /// 拍摄文件生成后的回调
 ///
+/// Return `true` if it has handled arguments.
+/// 如果在回调中已经进行了处理，请返回 `true`。
+///
 /// ### Notice about the implementation
-///  * After the callback is implemented, the default viewer page 
-///    will not be pushed anymore.
+///  * After the callback is implemented and returned `true`,
+///    the default viewer page will not be presented anymore.
 ///
 /// ### 在实现时需要注意
-///  * 实现该方法后，默认的的预览页面不会再出现。
+///  * 实现了该方法且返回 `true` 后，默认的预览页面不会再出现。
 /// {@endtemplate}
-typedef XFileCapturedCallback = void Function(XFile, CameraPickerViewType);
+typedef XFileCapturedCallback = bool Function(XFile, CameraPickerViewType);
